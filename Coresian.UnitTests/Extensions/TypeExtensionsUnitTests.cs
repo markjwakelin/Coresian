44185ebcacacc27c756a90a5d5d79503b9974e04
Matthew Malkin <m@m3z.net>
2018-02-17 19:47:52 +0000

    tests for exception handling

--- Coresian.UnitTests/Extensions/TypeExtensionsUnitTests.cs
+++ Coresian.UnitTests/Extensions/TypeExtensionsUnitTests.cs
@@ -1,7 +1,63 @@
-﻿namespace Coresian.UnitTests.Extensions
+﻿using System;
+using System.Collections.Generic;
+using Coresian.Extensions;
+using Xunit;
+
+namespace Coresian.UnitTests.Extensions
 {
     public class TypeExtensionsUnitTests
     {
+
+        [Fact]
+        public void NullTypeCanBeCastAsObjectThrowsArgumentException()
+        {
+            try
+            {
+                ((Type) null).CanBeCastAs(typeof(object));
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.Equal("thisType", ane.ParamName);
+            }
+        }
+
+        [Fact]
+        public void NullInstanceCanBeCastAsObjectThrowsArgumentException()
+        {
+            try
+            {
+                ((object) null).CanBeCastAs(typeof(object));
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.Equal("thisObject", ane.ParamName);
+            }
+        }
         
+        [Fact]
+        public void ObjectTypeCanBeCastAsNullThrowsArgumentException()
+        {
+            try
+            {
+                typeof(object).CanBeCastAs(null);
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.Equal("givenType", ane.ParamName);
+            }
+        }
+        
+        [Fact]
+        public void ObjectInstanceCanBeCastAsNullThrowsArgumentException()
+        {
+            try
+            {
+                new object().CanBeCastAs(null);
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.Equal("givenType", ane.ParamName);
+            }
+        }
     }
 }
