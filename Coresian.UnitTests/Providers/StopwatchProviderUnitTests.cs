6534f0368c1b2ad3d4b5a12047458a69d58f38e5
Matthew Malkin <matt@zercat.net>
2018-02-23 11:51:04 +0000

    Create StopwatchProviderUnitTests.cs

---
+++ Coresian.UnitTests/Providers/StopwatchProviderUnitTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Coresian.Providers;
+using Xunit;
+
+namespace Coresian.UnitTests.Providers
+{
+    
+    public class StopwatchProviderUnitTests
+    {
+    /*
+        [Fact]
+        public void WhenIGetTheLocalDateTimeItIsReturned()
+        {
+            var target = new DateTimeProvider();
+            
+            var stopwatch = target.GetStopwatch();
+            stopwatch.Start();
+
+            Assert.InRange(result, timeBefore, timeAfter);
+        }
+
+        [Fact]
+        public void WhenIGetTheUtcDateTimeItIsReturned()
+        {
+            var target = new DateTimeProvider();
+            
+            var timeBefore = DateTime.UtcNow;
+            var result = target.GetUtc();
+            var timeAfter = DateTime.UtcNow;
+
+            Assert.InRange(result, timeBefore, timeAfter);
+        }
+        */
+    }
+}
