14fa5fe1b55d43e1e0fa6efba764730cc58f0009
Matthew Malkin <matthew.malkin@motorpoint.co.uk>
2018-03-20 08:38:41 +0000

    added IConverter

---
+++ Coresian/Abstractions/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Coresian.Interfaces.Abstractions;
+
+namespace Coresian.Abstractions
+{
+    public class Converter : IConverter
+    {
+
+        public byte[] FromBase64(string content)
+        {
+            return Convert.FromBase64String(content);
+        }
+
+        public string ToBase64(byte[] content, int offset = 0, int? length = null, Base64FormattingOptions formattingOptions = Base64FormattingOptions.None)
+        {
+            if (!length.HasValue)
+                length = content.Length - offset;
+            return Convert.ToBase64String(content, offset, length.Value, formattingOptions);
+        }
+    }
+}
---
+++ Coresian/Interfaces/Abstractions/IConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coresian.Interfaces.Abstractions
+{
+    public interface IConverter
+    {
+        byte[] FromBase64(string content);
+
+        string ToBase64(byte[] content, int offset = 0, int? length = null,
+            Base64FormattingOptions formattingOptions = Base64FormattingOptions.None);
+    }
+}
