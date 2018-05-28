0b032048080288003abde3b7ec87081cdb592c94
Matthew Malkin <matthew.malkin@motorpoint.co.uk>
2018-05-29 00:09:40 +0100

    adds AsyncLock

---
+++ Coresian/Utility/AsyncLock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coresian.Utility
+{
+    //#TODO write up some unit tests
+    /// <summary>
+    /// Code published by Scott Hanselman based on code by Stephen Toub
+    /// http://www.hanselman.com/blog/ComparingTwoTechniquesInNETAsynchronousCoordinationPrimitives.aspx
+    /// https://blogs.msdn.microsoft.com/pfxteam/2012/02/12/building-async-coordination-primitives-part-6-asynclock/
+    /// </summary>
+    public sealed class AsyncLock
+    {
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        private readonly Task<IDisposable> _releaser;
+
+        public AsyncLock()
+        {
+            _releaser = Task.FromResult((IDisposable)new Releaser(this));
+        }
+
+        public Task<IDisposable> LockAsync()
+        {
+            var wait = _semaphore.WaitAsync();
+            return wait.IsCompleted ?
+                _releaser :
+                wait.ContinueWith((_, state) => (IDisposable)state,
+                    _releaser.Result, CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+
+        private sealed class Releaser : IDisposable
+        {
+            private readonly AsyncLock _toRelease;
+            internal Releaser(AsyncLock toRelease) { _toRelease = toRelease; }
+            public void Dispose() { _toRelease._semaphore.Release(); }
+        }
+    }
+}
